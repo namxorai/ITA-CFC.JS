be0c17f8c1c75225ce6211f63e7c5efda2dcf50f
Namxang Rai <namxo@gmail.com>
2024-07-01 15:12:42 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const friend1 = 'ila'
+const friend2 = 'bids'
+const friend3 = 'winner'
+
+const friends = ['ila,bids,winner']
+
+const name = "namxoo"
+const age = "0"
+const address = "Dharan"
+
+const me = {
+name : "namxo",
+age : '0',
+address : "Dharan",
+}
+
